71959428c9405144e048350564b5be13e62ad69a
YeLyuUT <y.lyu@utwente.nl>
2018-11-08 21:36:37 +0100

    fix wording directory of download.sh of DAVIS

--- data/DAVIS/download.sh
+++ data/DAVIS/download.sh
@@ -9,11 +9,17 @@ if [ ! -d "$DIRECTORY_DAVIS" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   echo "${DIRECTORY_DAVIS} does not exist, create it now..."
   mkdir ${DIRECTORY_DAVIS}
+  echo "Clone DAVIS17 repository"
+  git clone ${URL_DAVIS_2017} ${DIRECTORY_DAVIS}
+elif [ ! "$(ls -A ${DIRECTORY_DAVIS})"];then
+  # If directory is empty, git clone the repository
+  echo "${DIRECTORY_DAVIS} already exists, but is empty"
+  echo "Clone DAVIS17 repository"
   git clone ${URL_DAVIS_2017} ${DIRECTORY_DAVIS}
 else
   echo "${DIRECTORY_DAVIS} already exists, no need to download"
 fi
 
-cd "${DIR}/DAVIS17"
+cd "${DIR}/DAVIS17/data"
 # Download Davis17 dataset
-"./data/get_davis.sh"
+"./get_davis.sh"
--- lib/datasets/davis.py
+++ lib/datasets/davis.py
@@ -2,14 +2,16 @@
 
 import os
 from datasets.imdb import imdb
-import datasets.ds_utils as ds_utils
-import xml.etree.ElementTree as ET
 import numpy as np
-import scipy.sparse
-import scipy.io as sio
-import utils.cython_bbox
-import cPickle
-import subprocess
-import uuid
 
 
+class DAVIS(imdb):
+  def evaluate_vos(self):
+    pass
+
+  def prepareBoundingBoxs(self):
+    pass
+
+  #prepare training data
+  def prepare_data(self):
+    pass
